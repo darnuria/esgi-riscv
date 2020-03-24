305cb7d2cf68a2a7f17eab9ff973826b2eadd602
Axel Viala <axel.viala@darnuria.eu>
2020-03-24 17:38:17 +0100

    improve comments in 02 exercice.

--- exercices/01_syscall.asm
+++ exercices/01_syscall.asm
@@ -12,17 +12,19 @@
 .data
 
 # la directive .asciz permet de déclarer une chaine fini par un 0x00, '\0' en ascii.
-msg: .asciz "Hello ASM from .data"
+message: .asciz "Hello ASM from .data"
 
 .text # <- segment de code.
 
 # Exercice: Observation dans Rars
+#
 # - Quelle est l'addresse de base de la chaine msg? Son addresse de fin?
 # - Dans quelle segment de la mémoire est elle?
 # - Quelle sont les valeurs en hexadecimales présente dans .data?
 
+# la: Load Address, charge une adresse (label) dans un registre
 la a0, ??? # argument de l'appel systeme
-li a7, ??? # appel systeme 4 selon la doc de Rars printString
-ecall # Peret de basculer en mode kernel pour faire l'appel systeme.
+li a7, ??? # appel systeme numéro 4 selon la doc de Rars printString
+ecall      # Permet de basculer en mode kernel pour faire l'appel systeme
 
 # Bonus: Afficher maintenant un nombre, vous aurrez besoin de la documentation.
