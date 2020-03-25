57583542e1fd063741ee7743b4bfacf680ff35d2
Axel Viala <axel.viala@darnuria.eu>
2020-03-25 15:51:39 +0100

    Loop + array so much fun

---
+++ exercices/05_array_loop.asm
@@ -0,0 +1,38 @@
+# Exercice 3
+#
+# Objectif: Additionner toutes les cases d'un tableau avec une boucle!
+#
+# A faire: complêter le programme à trou.
+#
+# Questions:
+# A venir
+
+.data
+
+.text
+# Affichez la somme ici!
+
+###
+# Méthode avec une boucle
+###
+
+# t0: size <- *size_array
+??  ???, size_array
+# Il nous faut un registre pour la somme
+# a0: sum <- 0
+
+
+# Pour faire une boucle il nous faut l'addresse de base du tableau
+
+
+# Un label pour le début de la boucle
+for:
+# Ici un test pour savoir si o
+
+# Un label pour la sortie de boucle
+end_for:
+
+
+# A faire par vous totalement:
+# Afficher nos deux nombres dans la console
+# indice: syscall printInt
