afa7b0fa9f8e0a03868c62273e9a511af2d10b9f
Axel Viala <axel.viala@darnuria.eu>
2020-04-21 16:20:58 +0200

    Ajout d'un diagramme etc.

--- exercices/04_array.asm
+++ exercices/04_array.asm
@@ -3,7 +3,7 @@
 # Objectif: Additionner toutes les cases d'un tableau version «naive»
 # Sans boucle, ça correspond à faire l'addition de array_g[0] + array_g[1] + array_g[2].
 #
-# A faire: complêter le programme à trou.
+# A faire: compléter le programme à trou.
 #
 # Questions:
 #
@@ -11,7 +11,7 @@
 # - lw: À quoi sert l'immédiat devant le registre contenant l'adresse?
 # - Que va contenir le registre de destination de lw?
 #
-# - Avec une instruction comme add à quoi correspondent les trois registes?
+# - Avec une instruction comme add à quoi correspondent les trois registres?
 # `add reg0, reg1, reg2`
 #   - reg0:
 #   - reg1:
@@ -29,6 +29,14 @@
 size_array_g: .word 3
 array_g: .word 124, 256, 512
 
+# En mémoire ça resemble à ça @ référe dans ce schema a l'addresse
+# de base du tableau.
+# +----------+-------+---------------+
+# | Addresse | @ + 0 | @ + 4 | @ + 8 |
+# +----------+-------+-------+-------+
+# | Contenu  |  124  |  256  |  512  |
+# +----------+-------+-------+-------+
+
 # Chaines pour les test
 msg_success: .asciz "La somme correspond bien! Bravo."
 msg_fail: .asciz "Whops c'est pas encore ça, courage! :)"
@@ -37,17 +45,19 @@ msg_fail: .asciz "Whops c'est pas encore ça, courage! :)"
 
 main:
 
-la t0, array_g # t0 = @array_g;
-# On souhaite faire la somme du tableau! On va additionne toute les cases dans un registre.
+la t0, array_g # t0 contiens l'addresse du tableau
 
-# Ici on utilise le nombre devant le registre pour faire une petit calcul de mémoire.
+# Pour faire la somme du tableau on va additionner manuellement,
+# le contenu de toutes les cases du tableau.
+
 lw  t1, 0(t0) # t1: array_g[0]
 # lw ???, 4(???) <- TODO: À completer!
+# Vous pouvez aussi faire une addition sur l'addresse qui est dans t0
 add a0, t1, t2 # sum = t2 + t1
 
-# A vous de jouer il faut écrire deux instructions!
-# t1: array_g[2]
-# sum = sum + t1
+
+# Ici il vous reste à additionner a0 qui contiens la somme
+# avec le registre qui contiendra la valeur de la 3ème case du tableau.
 
 ###
 # Partie prof test si ça fonctionne et affiche un message en fonction.
